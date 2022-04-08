a04aced85e942c4db99ac107dd3b61797da2e2a5
Dawid T <dawid.trochimiuk@wp.pl>
2022-04-08 23:00:04 +0200

    changes in index.js

--- index.js
+++ index.js
@@ -1,48 +1,58 @@
 document.addEventListener("DOMContentLoaded", () => {
   const input = document.querySelector(".searchInput");
   const list = document.querySelector(".recipes");
+  const info = document.querySelector(".info");
 
-  let mealName = "chicken";
+  let recipesList = [];
 
-  let searchMeal = (e) => {
-    mealName = e.target.value;
-    console.log(mealName);
+  let mealList = (mealName) => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         let recipes = data.meals;
-        console.log(recipes);
+        // console.log(recipes);
+        recipesList.push(recipes);
+        // console.log(recipesList[0]);
+        input.value = "";
+        info.remove();
+        showList();
+      });
+  };
+  let showList = () => {
+    recipesList[0].forEach((recipe) => {
+      let newRecipe = document.createElement("div");
+      newRecipe.setAttribute("id", `${recipe.idMeal}`);
+      newRecipe.classList.add("recipe");
+      let image = document.createElement("div");
+      image.classList.add("image");
+      let img = document.createElement("img");
+      img.src = `${recipe.strMealThumb}`;
+      img.alt = "meal foto";
+      let title = document.createElement("div");
+      title.classList.add("recipe-title");
+      let h3 = document.createElement("h3");
+      h3.innerHTML = `${recipe.strMeal}`;
 
-        recipes.map((recipe) => {
-          const newRecipe = document.createElement("div");
-          newRecipe.setAttribute("id", `${recipe.idMeal}`);
-          newRecipe.classList.add("recipe");
-          newRecipe.innerHTML = `
-          <div class="content">
-            <div class="image">
-              <img
-                src=${recipe.strMealThumb}
-                alt="zdjęcie obiadu"
-                />
-                </div>
-            <div class="recipe-title">
-              <h3><a>${recipe.strMeal}</a></h3>
-            </div>
-            </div>
-          `;
-          list.appendChild(newRecipe);
-
-          // `<div class="recipe">
-          //   </div>`;
-        });
-      })
-      .catch(
-        (error = (e) => {
-          console.log(e);
-        })
-      );
+      newRecipe.appendChild(image);
+      image.appendChild(img);
+      newRecipe.appendChild(title);
+      title.appendChild(h3);
+      list.appendChild(newRecipe);
+      newRecipe.addEventListener("click", (e) => showDetail(recipe.idMeal));
+    });
   };
 
-  input.addEventListener("keyup", searchMeal);
+  let showDetail = (id) => {
+    console.log("recipe: ", id);
+  };
+
+  input.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13 && input.value !== "") {
+      mealName = e.target.value;
+      recipesList = [];
+      console.log(mealName);
+      mealList(mealName);
+    }
+  });
 });
